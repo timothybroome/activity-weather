5bf38454dbcb5ca987a81647975bfa768c794e12
Timothy Broome <timothy@broome.me>
2025-06-09 08:57:16 +0100

    configure tailwind, install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+
+
+/** @type { import('@storybook/nextjs-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs-vite",
+    "options": {}
+  },
+  "staticDirs": [
+    "../public"
+  ]
+};
+export default config;
